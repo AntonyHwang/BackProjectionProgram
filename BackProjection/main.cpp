bfeaa3c9ac4137834306b01c96e0bcff7ca3fd65
Antony Hwang <AntonyHwang@hotmail.com>
2016-10-24 10:58:16 +0100

    Initial Commit

---
+++ BackProjection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BackProjection
+//
+//  Created by Antony Hwang on 24/10/2016.
+//  Copyright © 2016 Antony Hwang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
