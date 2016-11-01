03c9a52a0d3e20deba7386484669026bc9665c79
Antony Hwang <AntonyHwang@hotmail.com>
2016-11-01 23:33:29 +0000

    3D points data added

--- BackProjection/main.cpp
+++ BackProjection/main.cpp
@@ -24,6 +24,12 @@ struct point2D {
     double y;
 };
 
+struct point3D {
+    double x;
+    double y;
+    double z;
+};
+
 struct cameraInfo {
     double focalLen;
     mat K;//IntrinsicMatrix 3x3
@@ -64,38 +70,37 @@ void readFile() {
                 cameraCount++;
                 //cout << "\n" << "Storing camera " << cameraCount << "\n";
                 camera[cameraCount - 1].focalLen = stringToDouble(line);
+                camera[cameraCount - 1].K(0,0) = camera[cameraCount - 1].focalLen;
                 camera[cameraCount - 1].K(1,1) = camera[cameraCount - 1].focalLen;
-                camera[cameraCount - 1].K(2,2) = camera[cameraCount - 1].focalLen;
-                //cout << "FocalLen: " << focalLen [cameraCount - 1] << "\n";
+                //cout << "K: \n" << camera[cameraCount - 1].K << "\n";
             }
             else if (lineCount == 4) {
                 //storing Translation T
                 double x, y, z;
                 camfile >> x >> y >> z;
                 camera[cameraCount - 1].T << x << endr << y << endr << z;
-                //cout << "T: " << T [cameraCount - 1][0][0] << " " << T [cameraCount - 1][0][1] << " " << T [cameraCount - 1][0][2] << "\n";
-                //cout << camera[cameraCount - 1].T << "\n";
+                //cout << "T: \n" << camera[cameraCount - 1].T << "\n";
             }
             else if (lineCount == 5) {
                 //storing Camera position C
                 double x1, y1, z1;
                 camfile >> x1 >> y1 >> z1;
                 camera[cameraCount - 1].C << x1 << endr << y1 << endr << z1;
-                //cout << "C: " << C [cameraCount - 1][0][0] << " " << C [cameraCount - 1][0][1] << " " << C [cameraCount - 1][0][2] << "\n";
+                //cout << "C: \n" << camera[cameraCount - 1].C << "\n";
             }
             else if (lineCount == 6) {
                 //storing Axis angle format of R
                 double x, y, z;
                 camfile >> x >> y >> z;
                 camera[cameraCount - 1].aR << x << endr << y << endr << z;
-                //cout << "aR: " << aR [cameraCount - 1][0][0] << " " << aR [cameraCount - 1][0][1] << " " << aR [cameraCount - 1][0][2] << "\n";
+                //cout << "aR: \n" << camera[cameraCount - 1].aR << "\n";
             }
             else if (lineCount == 7) {
                 //storign Quaternion format of R
                 double x, y, z, t;
                 camfile >> x >> y >> z >> t;
                 camera[cameraCount - 1].qR << x << endr << y << endr << z << endr << t;
-                //cout << "qR: " << qR [cameraCount - 1][0][0] << " " << qR [cameraCount - 1][0][1] << " " << qR [cameraCount - 1][0][2] << " " << qR [cameraCount - 1][0][3] << "\n";
+                //cout << "qR: \n" << camera[cameraCount - 1].qR << "\n";
             }
             else if (lineCount == 8) {
                 //storing Matrix format of R
@@ -113,13 +118,14 @@ void readFile() {
                 double z1, z2, z3;
                 camfile >> z1 >> z2 >> z3;
                 r2 << z1 << z2 << z3;
-                camera[cameraCount - 1].R = join_cols(r3, r2);
+                //camera[cameraCount - 1].R = join_cols(r3, r2);
+                //cout << "R: \n" << camera[cameraCount - 1].R << "\n";
             }
             else if (lineCount == 12) {
                 //storing Normalized radial distortion
                 stringstream convert(line);
                 convert >> camera[cameraCount - 1].rD;
-                //cout << "rD: " << rD [cameraCount - 1] << "\n";
+                //cout << "rD: \n" << camera[cameraCount - 1].rD << "\n";
             }
             if (lineCount > 14) {
                 lineCount = 1;
@@ -133,21 +139,39 @@ void readFile() {
     else cout << "Unable to open file";
 }
 
-string getFileName(int cameraNum) {
-    stringstream convert;
-    convert << cameraNum;
-    string fileName = convert.str();
-    fileName = fileName + ".txt";
-    while (fileName.length() < 12) {
-        fileName = '0' + fileName;
+string getFileName(string fileType, int index) {
+    string fileName;
+    if (fileType == "PMatrix") {
+        stringstream convert;
+        convert << index;
+        fileName = convert.str();
+        fileName = fileName + ".txt";
+        while (fileName.length() < 12) {
+            fileName = '0' + fileName;
+        }
+        return fileName;
     }
-    return fileName;
+    else if (fileType == "Patch") {
+        //option-0001.patch
+        stringstream convert;
+        convert << index;
+        fileName = convert.str();
+        fileName = fileName + ".patch";
+        while (fileName.length() < 10) {
+            fileName = '0' + fileName;
+        }
+        return "option-" + fileName;
+    }
+    else {
+        cout << "cannot find name for this filetype";
+        return "file not exist";
+    }
 }
 
 void readPFiles() {
     for (int currentCamera = 0; currentCamera < cameraCount; currentCamera++) {
         std::string line;
-        ifstream pfile (getFileName(currentCamera));
+        ifstream pfile (getFileName("PMatrix", currentCamera));
         if (pfile.is_open()) {
             while (pfile >> line) {
                     double x1, x2, x3, x4,
@@ -157,18 +181,54 @@ void readPFiles() {
                     pfile >> x1 >> x2 >> x3 >> x4
                           >> y1 >> y2 >> y3 >> y4
                           >> z1 >> z2 >> z3 >> z4;
-                    //cout << "\n" << lineCount << "\n";
                     
                     camera[currentCamera].P << x1 << x2 << x3 << x4 << endr
                                             << y1 << y2 << y3 << y4 << endr
                                             << z1 << z2 << z3 << z4;
             }
             pfile.close();
-            //cout << camera[currentCamera].P << "\n";
+            //cout << "P: \n" << camera[currentCamera].P << "\n";
         }
         else cout << "Unable to open file";
     }
 }
+
+void readPatchFile() {
+    std::string line;
+    std::string word;
+    int currentFile = 0;
+    string fileName;
+    vec point_3D;
+    lineCount = 0;
+    while (fileName != "file not exist") {
+        fileName = getFileName("Patch", currentFile);
+        ifstream patchfile (fileName);
+        if (patchfile.is_open()) {
+            while (getline(patchfile, line)) {
+                patchfile >> word;
+                if (word == "PATCHS") {
+                    lineCount = 1;
+                }
+                else if (lineCount == 1) {
+                    double x, y, z;
+                    patchfile >> x >> y >> z;
+                    point_3D << stringToDouble(word) << x << y << z;
+                    cout << fileName << "\n";
+                    cout << point_3D << "\n";
+                    lineCount++;
+                }
+                else if (lineCount == 5) {
+                    
+                }
+                if (patchfile.eof()) {
+                    patchfile.close();
+                    currentFile++;
+                }
+            }
+        }
+    }
+}
+
 
 void calculate2DPoint(){
     //calculate points
@@ -177,8 +237,9 @@ void calculate2DPoint(){
 
 int main () {
     initialiseK();
-    readFile();
-    readPFiles();
+    readFile();//contains camera information
+    readPFiles();//contains the camera matrix of each camera
+    readPatchFile();//contains the the 3D coordinates
     //cout << camera[0].R << "\n";
     calculate2DPoint();
     
